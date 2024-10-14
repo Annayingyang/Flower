1f7d024024615b0dafb37b4b44a72103a420f2a7
Annayingyang <2540640@students.wits.ac.za>
2024-10-14 21:33:47 +0200

    idk

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.querySelector('.navigate-button');
+    
+    button.addEventListener('click', function(event) {
+        console.log('Button clicked!'); // Log to console for testing
+    });
+});
